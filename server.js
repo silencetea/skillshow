67a9113d814ed568931f055e65f9f6371ec2c758
silencetea <silencetea@yeah.net>
2020-02-14 23:45:54 +0800

    modify server.js

--- server.js
+++ server.js
@@ -8,15 +8,6 @@ const {
 } = require('vue-server-renderer')
 const isProd = process.env.NODE_ENV === 'production'
 
-/*const template = require('fs').readFileSync('/path/to/index.template.html', 'utf-8')
-const serverBundle = require('/path/to/vue-ssr-server-bundle.json')
-const clientManifest = require('/path/to/vue-ssr-client-manifest.json')
-
-const renderer = createRenderer(serverBundle, {
-    template,
-    clientManifest
-})
-*/
 let renderer
 let readyPromise
 const templatePath = resolve('./src/index.template.html')
@@ -28,12 +19,12 @@ function createRenderer(bundle, options) {
     // https://github.com/vuejs/vue/blob/dev/packages/vue-server-renderer/README.md#why-use-bundlerenderer
     return createBundleRenderer(bundle, Object.assign(options, {
         // for component caching
-        cache: LRU({
+        /*cache: LRU({
             max: 1000,
             maxAge: 1000 * 60 * 15
-        }),
+        }),*/
         // this is only needed when vue-server-renderer is npm-linked
-        //basedir: resolve('./dist'),
+        // basedir: resolve('./dist'),
         // recommended for performance
         runInNewContext: false
     }))
@@ -43,7 +34,7 @@ if (isProd) {
 
     const bundle = require('./dist/vue-ssr-server-bundle.json')
     const clientManifest = require('./dist/vue-ssr-client-manifest.json')
-    renderer = createBundleRenderer(bundle, {
+    renderer = createRenderer(bundle, {
         template,
         clientManifest
     })
@@ -53,7 +44,7 @@ if (isProd) {
         server,
         templatePath,
         (bundle, options) => {
-            renderer = createBundleRenderer(bundle, options)
+            renderer = createRenderer(bundle, options)
         }
     )
 }
